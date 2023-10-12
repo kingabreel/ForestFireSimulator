7dfb6e3e7c9a182aaefb4e7d988177d12915cb42
kingabreel <gabrielyury441@gmail.com>
2023-10-12 12:27:06 -0300

    Initial commit, View implemented

---
+++ src/ForestView.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class ForestView extends JFrame {
+
+    public ForestView(){
+        this.setTitle("Forest Fire Simulator");
+        this.setSize(1240, 720);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
