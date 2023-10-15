7aaea8e77788a7f9cb4dbaeedf0cd1e80a419d60
kingabreel <gabrielyury441@gmail.com>
2023-10-15 11:50:04 -0300

    dynamic painting board

--- src/ForestView.java
+++ src/ForestView.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
 
 public class ForestView extends JFrame {
     JPanel configPanel;
     JPanel board;
+    private int WIDTH;
+    private int HEIGHT;
 
     public ForestView(){
         this.setTitle("Forest Fire Simulator");
@@ -20,18 +25,24 @@ public class ForestView extends JFrame {
     private void setConfigsPanel(){
         JPanel panel = new JPanel(new FlowLayout());
 
-        JButton buttonSmall = new JButton("small (100km²)");
-        JButton buttonMedium = new JButton("Medium (300km²)");
-        JButton buttonBig = new JButton("small (1000km²)");
+        JButton buttonSmall = new JButton("small (50km²)");
+        JButton buttonMedium = new JButton("Medium (106km²)");
+        JButton buttonBig = new JButton("small (122km²)");
 
         buttonSmall.addActionListener(e -> {
-            createBoard(new GridLayout(1000, 1000));
+            this.HEIGHT = 720;
+            this.WIDTH = 1000;
+            createBoard(new GridLayout(50, 50));
         });
         buttonMedium.addActionListener(e -> {
-            createBoard(new GridLayout(3000, 3000));
+            this.HEIGHT = 3000;
+            this.WIDTH = 3000;
+            createBoard(new GridLayout(75, 150));
         });
         buttonBig.addActionListener(e -> {
-            createBoard(new GridLayout(10000, 10000));
+            this.HEIGHT = 5000;
+            this.WIDTH = 5000;
+            createBoard(new GridLayout(75, 200));
         });
 
         panel.add(buttonSmall);
@@ -44,10 +55,38 @@ public class ForestView extends JFrame {
     private void createBoard(GridLayout g){
         this.remove(configPanel);
         this.setSize(1240, 720);
-        JPanel panel = new JPanel(g);
+        JPanel panel = paintBoard(new JPanel(g));
 
-        panel.setBackground(Color.BLACK);
-
+        repaint();
         this.add(panel);
+        this.pack();
+    }
+
+    private JPanel paintBoard(JPanel p){
+        JPanel panel = p;
+        Random random = new Random();
+        System.out.println("ok");
+
+        for(int i = 0; i < 100; i++){
+            for (int j = 0; j <= i; j++){
+                JPanel cell = new JPanel();
+                cell.setPreferredSize(new Dimension(10, 10));
+                cell.setBackground(Color.GREEN);
+
+                cell.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+
+                cell.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        if (cell.getBackground() == Color.GREEN) cell.setBackground(Color.RED);
+                        else if (cell.getBackground() == Color.RED) cell.setBackground(Color.BLUE);
+                        else cell.setBackground(Color.GREEN);
+                    }
+                });
+                repaint();
+                panel.add(cell);
+            }
+        }
+        return panel;
     }
 }
